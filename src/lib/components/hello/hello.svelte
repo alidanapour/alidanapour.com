43c7f37c1df84155101e62fcc35d2b38cbf4e984
Ali Danapour <ali.danapour@proton.me>
2024-07-17 17:17:33 -0700

    added inview

--- src/lib/components/hello/hello.svelte
+++ src/lib/components/hello/hello.svelte
@@ -1,5 +1,21 @@
 <script lang="ts">
+  import { inview } from 'svelte-inview';
+  import type { ObserverEventDetails, Options } from 'svelte-inview';
+  import { fly } from 'svelte/transition';
+  import { quintOut } from 'svelte/easing';
 
+  let isInView: boolean;
+  const options: Options = {
+    unobserveOnEnter: false,
+  };
+
+  const handleChange = ({ detail }: CustomEvent<ObserverEventDetails>) => {
+    isInView = detail.inView;
+    console.log("here! ", isInView);
+  };
 </script>
-
-<p class="text-5xl">Hi there!</p>
+<div use:inview={options} on:inview_change={handleChange}>
+  {#if isInView}
+    <p class="text-5xl" transition:fly={{ delay: 250, duration: 300, x: 100, y: 500, opacity: 0.5, easing: quintOut }}>Hi there!</p>
+  {/if}
+</div>
